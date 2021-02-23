6c2c2d32705fb7c7fcbfbf896b0ee198b71e5a2c
akhan2 <akhan2@adcs-MacBook-Pro.local>
2021-02-23 22:26:38 +0000

    Added gradients

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-
 // Create a new list item when clicking on the "Add" button
 function newAdd() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var textNode = document.createTextNode(inputValue);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var textNode = document.createTextNode(inputValue);
 
-    li.appendChild(textNode);
+  li.appendChild(textNode);
 
-    if (inputValue === '') {
-        alert("Please type something...");
-    } else {
-        document.getElementById("myUL").appendChild(li);
+  if (inputValue === '') {
+    alert("Please type something...");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("span");
+  var xSymbol = document.createTextNode("\u00D7");
+
+  span.className = "close";
+  span.appendChild(xSymbol);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      this.parentElement.div.style.display = "none";
     }
-    document.getElementById("myInput").value = "";
-
-    var span = document.createElement("span");
-    var xSymbol = document.createTextNode("\u00D7");
-
-    span.className = "close";
-    span.appendChild(xSymbol);
-    li.appendChild(span);
+  }
 }
 
 // Add a "check" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === "li") {
-        ev.target.classList.toggle('checked');
-    }
+var list = document.querySelector("ul");
+
+list.addEventListener('click', function (event) {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("checked");
+  }
 }, false);
 
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("li");
-for (var i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("Span");
-    var txt = document.createTextNode("\u00D7");
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
 }
